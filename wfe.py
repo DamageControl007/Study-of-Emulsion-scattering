2a5e71a7a991038888ffd842a41ae5d889d39578
DamageControl007 <somendra07jadon@gmail.com>
2021-08-27 18:36:29 +0530

    Norm path values

--- wfe.py
+++ wfe.py
@@ -57,7 +57,7 @@ dv=100
 
 urange = arr.array('d',[0.001,0.002,0.003,0.004,0.005,0.006,0.007,0.008,0.009,0.01,0.02,0.03,0.04,0.05,0.06,0.07,0.08,0.09,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1,2,3,4,5,6,7,8,9,10,20,30,40,50,60,70,80,90,100])
 
-for g in [0.1] :
+for g in [-1, -0.5, -0.1, 0.1, 0.5, 0.9, 0.99, 1] :
     #if g<xxx : continue
     for Ut in urange :
         if g==xxx and Ut<yyy : continue
@@ -495,7 +495,7 @@ for g in [0.1] :
                            w=w*m
                        else:
                            w=0
-        
+
         file_path = open("normpath.txt","a")
         file_path.write(str(g)+"\t")
         file_path.write(str(Ut)+"\t")
