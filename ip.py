723b3410b2bf95df6f1c7dcb01574bd85d9bb21e
Rohan Garg <99grohan@gmail.com>
2021-10-28 12:45:46 +0530

    Create ip.py

---
+++ ip.py
@@ -0,0 +1,22 @@
+import numpy as np
+import random
+import array as arr
+import matplotlib.pyplot as plt
+import miepython as mp
+import csv
+import xlrd
+import xlsxwriter
+import sys
+from datetime import date
+from pysolar.solar import *
+import datetime
+from solarpy import irradiance_on_plane
+import seaborn as sns
+import pandas as pd
+from matplotlib import cm
+from colorspacious import cspace_converter
+from collections import OrderedDict
+from collections import defaultdict
+
+def yip():
+	
